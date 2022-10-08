8b6141fce5ed453a263eeb9fde3634046ba0d2e0
hyeonho <6190606@naver.com>
2022-10-08 16:14:40 +0900

    populate-data-when-starting-up

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ create table person(
     birth_data timestamp ,
     primary key(id)
 );
+
+insert into person values(10001, 'ranga', 'hyderabad', LOCALTIMESTAMP);
+insert into person values(10002, 'james', 'new york', LOCALTIMESTAMP);
+insert into person values(10003, 'susan', 'amsterdam', LOCALTIMESTAMP);
