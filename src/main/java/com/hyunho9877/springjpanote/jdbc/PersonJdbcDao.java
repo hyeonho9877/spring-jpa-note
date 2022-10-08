1ba6aee9ff21169c89800e45e8080f17dd3ad393
hyeonho <6190606@naver.com>
2022-10-08 16:22:23 +0900

    jdbc-query-findAll

---
+++ src/main/java/com/hyunho9877/springjpanote/jdbc/PersonJdbcDao.java
@@ -0,0 +1,22 @@
+package com.hyunho9877.springjpanote.jdbc;
+
+import com.hyunho9877.springjpanote.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    //select * from person
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+}
