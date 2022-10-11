a98cf0234c6df7d2f075f2449252b68a753b9af1
hyeonho <6190606@naver.com>
2022-10-11 21:54:16 +0900

    spring-data-jpa-crud

---
+++ jpa-hibernate/src/test/java/com/hyunho9877/jpahibernate/repository/CourseSpringDataRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.hyunho9877.jpahibernate.repository;
+
+import com.hyunho9877.jpahibernate.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class CourseSpringDataRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseSpringDataRepository repository;
+
+    @Test
+    void findById_coursePresent() {
+        Optional<Course> course = repository.findById(10001L);
+        logger.info("course -> {}", course.isPresent());
+        assertTrue(course.isPresent());
+    }
+
+    @Test
+    void findById_courseNotPresent() {
+        Optional<Course> course = repository.findById(20001L);
+        assertFalse(course.isPresent());
+    }
+
+    @Test
+    void playAround() {
+        /*Course course = new Course("course");
+        repository.save(course);
+        course.setName("course updated");
+        repository.save(course);*/
+
+        logger.info("courses -> {}", repository.findAll());
+        logger.info("count -> {}", repository.count());
+    }
+
+}
