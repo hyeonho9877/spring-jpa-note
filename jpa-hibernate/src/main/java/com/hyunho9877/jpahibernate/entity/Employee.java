d12d472bb321f4c323d8f45a2ee05029f905c4d4
hyeonho <6190606@naver.com>
2022-10-11 16:39:56 +0900

    jpa-inheritance-join

--- jpa-hibernate/src/main/java/com/hyunho9877/jpahibernate/entity/Employee.java
+++ jpa-hibernate/src/main/java/com/hyunho9877/jpahibernate/entity/Employee.java
@@ -3,7 +3,7 @@ package com.hyunho9877.jpahibernate.entity;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Employee {
     @Id
     @GeneratedValue
